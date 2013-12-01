a2eb518adaee58d6b1bccd913dfb548d5e2c912a
NovemberFoxtrot <dothehustle@hush.com>
2013-12-01 20:04:19 +0900

    Update fib_test.go

--- src/fib/fib_test.go
+++ src/fib/fib_test.go
@@ -36,7 +36,6 @@ func BenchmarkFib3(b *testing.B)  { benchmarkFib(3, b) }
 func BenchmarkFib10(b *testing.B) { benchmarkFib(10, b) }
 func BenchmarkFib20(b *testing.B) { benchmarkFib(20, b) }
 func BenchmarkFib40(b *testing.B) { benchmarkFib(40, b) }
-func BenchmarkFib10000(b *testing.B) { benchmarkFib(10000, b) }
 
 var result int
 
