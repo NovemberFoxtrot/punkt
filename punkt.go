96b8ff0abda8c4d52540679abab117880575468b
NovemberFoxtrot <november@fox.trot>
2013-12-02 01:44:10 -0800

    rm junk

--- punkt.go
+++ punkt.go
@@ -3,7 +3,6 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/NovemberFoxtrot/punkt/fib"
 	"github.com/NovemberFoxtrot/punkt/server"
 	"net/http"
 )
@@ -16,8 +15,6 @@ func main() {
 	if *theVersion == true {
 		fmt.Println("0.0.1")
 	}
-
-	fmt.Println(fib.Fib(2))
 
 	http.HandleFunc("/", server.Index)
 	http.ListenAndServe(":8080", nil)
