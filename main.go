c07019c184ea5ff58c4a5104e4c5362e1918b301
NovemberFoxtrot <november@fox.trot>
2014-04-15 16:36:44 +0900

    two

--- main.go
+++ main.go
@@ -43,7 +43,7 @@ type entry struct {
 func Index(w http.ResponseWriter, r *http.Request) {
 	data := []entry{
 		{"title", "body"},
-		{"one", "two"},
+		{"one", "ttwotwotwotwotwotwotwotwotwotwotwotwotwotwotwotwotwotwotwotwotwotwotwotwotwotwotwotwotwotwotwotwotwotwotwotwotwotwowo"},
 	}
 
 	if r.Method == "POST" {
